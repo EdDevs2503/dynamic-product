aeb0d572d67b77fae1593a553615b6c7d6f49565
edissonmata <PumbaYTimon123@>
2022-11-03 23:11:38 -0400

    optimize colors

--- src/common/colors.ts
+++ src/common/colors.ts
@@ -1,4 +1,4 @@
-export const black: string = 'rgba(64, 64, 64, 1)'
-export const blue: string = 'rgba(0, 71, 108, 1)'
-export const white: string = 'rgba(255, 255, 255, 1)'
-export const aqua: string = 'rgba(229, 236, 240, 1)'
+export const black: string = 'rgb(64, 64, 64)'
+export const blue: string = 'rgb(0, 71, 108)'
+export const white: string = 'rgb(255, 255, 255)'
+export const aqua: string = 'rgb(229, 236, 240)'
