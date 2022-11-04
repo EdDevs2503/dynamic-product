c8e351ce38ea3eb567a0cb9279783462827eaafe
edissonmata <PumbaYTimon123@>
2022-11-04 13:58:57 -0400

    Feature visual issue bugfix

--- src/components/feature/index.tsx
+++ src/components/feature/index.tsx
@@ -10,8 +10,10 @@ export interface props {
 
 export const Feature = ({ title, description, Icon }: props): ReactElement => {
   return (
-    <div className='flex flex-col items-center space-y-2 mb-4'>
-        <Icon fill={colors.blue} />
+    <div className='flex flex-col justify-between items-center space-y-2 max-[760px]:mb-4'>
+        <div className='flex h-11 items-center justify-center'>
+          <Icon fill={colors.blue} />
+        </div>
         <div className='flex flex-col items-center'>
             <DynamicText type='h2' color={colors.blue} content={title} weight="bold" />
             <DynamicText type='normal' color={colors.black} content={description} weight="normal" />
