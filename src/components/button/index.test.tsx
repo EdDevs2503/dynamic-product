d5f084fb7a3f5fd3709bae89dfb5b7a109878d7e
edissonmata <PumbaYTimon123@>
2022-11-03 23:13:36 -0400

    button

---
+++ src/components/button/index.test.tsx
@@ -0,0 +1,66 @@
+import { render, screen, fireEvent } from '@testing-library/react'
+import { colors } from '../../common'
+import Button, { props } from './index'
+
+describe('Testing button component', () => {
+  test('Given button content is rendering', () => {
+    const fakeProps: props = {
+      content: 'Test content',
+      isBG: false,
+      onClick: () => {}
+    }
+    render(<Button {...fakeProps} />)
+    const text = screen.getByText(fakeProps.content)
+
+    expect(text).toBeInTheDocument()
+  })
+
+  test('Test if isBG is false', () => {
+    const fakeProps: props = {
+      content: 'Test content',
+      isBG: false,
+      onClick: () => {}
+    }
+    render(<Button {...fakeProps} />)
+
+    const button = screen.getByRole('button')
+    const text = screen.getByText(fakeProps.content)
+
+    const buttonStyles = window.getComputedStyle(button)
+    const textStyles = window.getComputedStyle(text)
+
+    expect(buttonStyles.backgroundColor).toBe('transparent')
+    expect(textStyles.color).toBe(colors.blue)
+  })
+
+  test('Test if isBG is true', () => {
+    const fakeProps: props = {
+      content: 'Test content',
+      isBG: true,
+      onClick: () => {}
+    }
+    render(<Button {...fakeProps} />)
+
+    const button = screen.getByRole('button')
+    const text = screen.getByText(fakeProps.content)
+
+    const buttonStyles = window.getComputedStyle(button)
+    const textStyles = window.getComputedStyle(text)
+
+    expect(buttonStyles.backgroundColor).toBe(colors.blue)
+    expect(textStyles.color).toBe(colors.white)
+  })
+
+  test('Test onClick', () => {
+    const fakeProps: props = {
+      content: 'Test content',
+      isBG: true,
+      onClick: jest.fn()
+    }
+    render(<Button {...fakeProps} />)
+
+    const button = screen.getByRole('button')
+    fireEvent.click(button)
+    expect(fakeProps.onClick).toHaveBeenCalledTimes(1)
+  })
+})
---
+++ src/components/button/index.tsx
@@ -0,0 +1,23 @@
+import { ReactElement, SyntheticEvent } from 'react'
+import { colors } from '../../common'
+import DynamicText from '../dynamicText'
+
+export interface props {
+  content: string
+  isBG: boolean
+  onClick: (event: SyntheticEvent) => void
+}
+
+const Button = ({ content, isBG, onClick }: props): ReactElement => {
+  return (
+    <button
+        onClick={onClick}
+        className='w-44 h-10 rounded border'
+        style={{ borderColor: colors.blue, backgroundColor: isBG ? colors.blue : 'transparent' }}
+    >
+        <DynamicText type="normal" color={isBG ? colors.white : colors.blue} weight="extraBold" content={content} />
+    </button>
+  )
+}
+
+export default Button
