b629658bbeead9801cc2805d8bb081115e9be6e9
edissonmata <PumbaYTimon123@>
2022-11-04 12:23:03 -0400

    bugfix Price component - UI issues

--- src/components/price/index.tsx
+++ src/components/price/index.tsx
@@ -1,5 +1,5 @@
 import { ReactElement } from 'react'
-import { colors } from '../../common'
+import { colors, texts } from '../../common'
 import { DynamicText } from '../dynamicText'
 
 export interface props {
@@ -11,11 +11,17 @@ export const Price = ({ offerPrice, fullPrice }: props): ReactElement => {
   return (
     <div className='flex flex-col items-center space-y-1'>
         <div className='px-6 py-2 rounded inline-block' style={{ backgroundColor: colors.aqua }}>
-            <DynamicText type='normal' weight='bold' content={`Today’s Total*: $${offerPrice}`} />
+            <DynamicText type='normal' weight='bold' color={colors.black} content={`Today’s Total*: $${offerPrice}`} />
         </div>
         <div className='flex justify-items-center space-x-1'>
-            <DynamicText type='tiny' weight='normal' content={'After the first billing cycle, your new plan will renew at'} />
-            <DynamicText type='tiny' weight='bold' content={`$${fullPrice} / term`} />
+            <p style={{
+              color: colors.black,
+              fontWeight: texts.textWeight.normal,
+              fontSize: texts.textSize.tiny,
+              textAlign: 'center'
+            }}>
+              After the first billing cycle, your new plan will renew at <span style={{ fontWeight: texts.textWeight.bold }}>{`$${fullPrice} / term`}</span>
+            </p>
         </div>
     </div>
   )
