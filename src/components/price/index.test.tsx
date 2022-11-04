9a08cf33a3d9c8ef3ad08fdb1951ca202a4b776b
edissonmata <PumbaYTimon123@>
2022-11-03 21:40:49 -0400

    Price component finished

---
+++ src/components/price/index.test.tsx
@@ -0,0 +1,24 @@
+import { render, screen } from '@testing-library/react'
+import Price, { props } from './index'
+
+describe('Price component', () => {
+  test('Is offer price is rendering', () => {
+    const fakeProps: props = {
+      offerPrice: 6.99,
+      fullPrice: 9.99
+    }
+    render(<Price {...fakeProps} />)
+    const text = screen.getByText(`Today’s Total*: $${fakeProps.offerPrice}`)
+    expect(text).toBeInTheDocument()
+  })
+
+  test('Is full price is rendering', () => {
+    const fakeProps: props = {
+      offerPrice: 6.99,
+      fullPrice: 9.99
+    }
+    render(<Price {...fakeProps} />)
+    const text = screen.getByText(`$${fakeProps.fullPrice} / term`)
+    expect(text).toBeInTheDocument()
+  })
+})
